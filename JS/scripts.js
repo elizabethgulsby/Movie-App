f88516b4a6ebc3cd8d5ee348b1a459ad50c6ec95
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2016-12-21 10:13:19 -0500

    Init commit

---
+++ JS/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	console.log("test");
+
+	// base url for all api calls
+	var apiBaseUrl = 'http://api.themoviedb.org/3';
+
+	// The base URL for image
+	var imageBaseUrl = 'http://image.tmdb.org/t/p/';
+
+	const nowPlayingUrl = apiBaseUrl + '/movies/now_playing?api_key=' + apiKey;
+
+
+
+});
