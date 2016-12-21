3c8147b67f125c278d1c760edbb2a156436208f9
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2016-12-21 16:55:50 -0500

    Added AJAX call for genres and text input in navbar

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,6 @@
 /*Version 1: 
 
-1) Now Playing results show on load.  If another display is desired for the landing page in future versions, the data from that respective AJAX call will need to be provided in place of Now Playing data.  
+1) Now Playing results show on load.  If another set of results are desired for the landing page, the data from that respective AJAX call will need to be provided in place of Now Playing data.  
 */
 
 $(document).ready(function() {
@@ -10,8 +10,14 @@ $(document).ready(function() {
 	});
 
 //VARIABLES FOR USE IN URLS, FUNCTIONS
-	//
-		// var movieID = 
+	//get value of input field
+	var input = $('#input').val();
+
+	//get movie by title (regex? partial title?)
+
+	//get movie by actor (regex? partial name?)
+
+
 
 //BASE URLS
 
@@ -29,7 +35,7 @@ $(document).ready(function() {
 	// console.log(peopleBaseUrl);
 
 	//base URL for titles (need regex for partial titles?)
-		var titleBaseUrl = ''
+		// var titleBaseUrl = '';
 
 	//Now-playing URL
 		const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key=' + apiKey;
@@ -47,7 +53,7 @@ $(document).ready(function() {
 				console.log(poster);
 				nowPlayingHTML += '<div class="col-sm-3">';
 					nowPlayingHTML += '<img src="' + poster + '">';
-					nowPlayingHTML += '<div></div>';
+					// nowPlayingHTML += '<div></div>';
 				nowPlayingHTML += '</div>';
 			}
 			//populates #movie-grid with HTML constructed above
@@ -57,8 +63,12 @@ $(document).ready(function() {
 	//genre array - to loop through for assignment to film results
 		var genres = [
 		    {
-		      "id": 10759,
-		      "name": "Action & Adventure"
+		      "id": 28,
+		      "name": "Action"
+		    },
+		    {
+		      "id": 12,
+		      "name": "Adventure"
 		    },
 		    {
 		      "id": 16,
@@ -85,53 +95,163 @@ $(document).ready(function() {
 		      "name": "Family"
 		    },
 		    {
-		      "id": 10762,
-		      "name": "Kids"
+		      "id": 14,
+		      "name": "Fantasy"
+		    },
+		    {
+		      "id": 36,
+		      "name": "History"
+		    },
+		    {
+		      "id": 27,
+		      "name": "Horror"
+		    },
+		    {
+		      "id": 10402,
+		      "name": "Music"
 		    },
 		    {
 		      "id": 9648,
 		      "name": "Mystery"
 		    },
 		    {
-		      "id": 10763,
-		      "name": "News"
+		      "id": 10749,
+		      "name": "Romance"
 		    },
 		    {
-		      "id": 10764,
-		      "name": "Reality"
+		      "id": 878,
+		      "name": "Science Fiction"
 		    },
 		    {
-		      "id": 10765,
-		      "name": "Sci-Fi & Fantasy"
+		      "id": 10770,
+		      "name": "TV Movie"
 		    },
 		    {
-		      "id": 10766,
-		      "name": "Soap"
+		      "id": 53,
+		      "name": "Thriller"
 		    },
 		    {
-		      "id": 10767,
-		      "name": "Talk"
-		    },
-		    {
-		      "id": 10768,
-		      "name": "War & Politics"
+		      "id": 10752,
+		      "name": "War"
 		    },
 		    {
 		      "id": 37,
 		      "name": "Western"
 		    }
-		];
+  		];
 
+  	//click listeners - call getGenre() on each click to retrieve AJAX call for genre-specific data and change html of genre-description class to match corresponding genre
+  	$('#28').click(function() {
+  		getGenre(28);
+  		$('.genre-description').html("Action");
+  	});
 
+  	$('#12').click(function() {
+  		getGenre(12);
+  		$('.genre-description').html("Adventure");
+  	});	
 
-	//AJAX call for genres
+  	$('#16').click(function() {
+  		getGenre(16);
+  		$('.genre-description').html("Animation");
+  	});	
+
+  	$('#35').click(function() {
+  		getGenre(35);
+  		$('.genre-description').html("Comedy");
+  	});	
+
+  	$('#80').click(function() {
+  		getGenre(80);
+  		$('.genre-description').html("Crime");
+  	});	
+
+  	$('#99').click(function() {
+  		getGenre(99);
+  		$('.genre-description').html("Documentary");
+  	});	
+
+  	$('#18').click(function() {
+  		getGenre(18);
+  		$('.genre-description').html("Drama");
+  	});	
+
+  	$('#10751').click(function() {
+  		getGenre(10751);
+  		$('.genre-description').html("Family");
+  	});	
+
+  	$('#14').click(function() {
+  		getGenre(14);
+  		$('.genre-description').html("Fantasy");
+  	});	
+
+  	$('#36').click(function() {
+  		getGenre(36);
+  		$('.genre-description').html("History");
+  	});	
+
+  	$('#27').click(function() {
+  		getGenre(27);
+  		$('.genre-description').html("Horror");
+  	});	
+
+  	$('#10402').click(function() {
+  		getGenre(10402);
+  		$('.genre-description').html("Music");
+  	});
+
+  	$('#9648').click(function() {
+  		getGenre(9648);
+  		$('.genre-description').html("Mystery");
+  	});
+
+  	$('#10749').click(function() {
+  		getGenre(10749);
+  		$('.genre-description').html("Romance");
+  	});
+
+  	$('#878').click(function() {
+  		getGenre(878);
+  		$('.genre-description').html("Science Fiction");
+  	});
+
+  	$('#10770').click(function() {
+  		getGenre(10770);
+  		$('.genre-description').html("TV Movie");
+  	});
+
+  	$('#53').click(function() {
+  		getGenre(53);
+  		$('.genre-description').html("Thriller");
+  	});
+
+  	$('#10752').click(function() {
+  		getGenre(10752);
+  		$('.genre-description').html("War");
+  	});
+
+  	$('#37').click(function() {
+  		getGenre(37);
+  		$('.genre-description').html("Western");
+  	});
+
+	//AJAX call for genres placed into a function; genre ID passed in from click listener
 		function getGenre(genreID) {
-			var genreIDUrl = 'http://api.themoviedb.org/3/genre/' + genreID + 'movies?api_key=' + apiKey + '&language=en-US&include_adult=false&sort_by=created_at.asc';
+			//constructing URL to get genre data
+			var genreIDUrl = 'https://api.themoviedb.org/3/genre/' + genreID + '/movies?api_key=' + apiKey + '&language=en-US&include_adult=false&sort_by=created_at.asc';
+				var genreHTML = '';
 			 	$.getJSON(genreIDUrl, function(genreData) {
-
-			 	})
+			 		console.log(genreData);
+			 		for (let i = 0; i < genreData.results.length; i++) {
+			 			var genrePoster = imageBaseUrl + 'w300' + genreData.results[i].poster_path;
+			 			genreHTML += '<div class="col-sm-3">';
+			 				genreHTML += '<img src="' + genrePoster + '">';
+			 			genreHTML += '</div>';
+			 		}
+			 		console.log(genreHTML);
+			 		$('#movie-grid').html(genreHTML);
+			 	});
 	 	}
-
-	// })
 
 });
